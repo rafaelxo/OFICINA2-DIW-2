77633678db4793b296b13dde399d020b9d11ad33
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-05-28 04:10:15 +0000

    Exemplo

--- exemplo/js/componentes/corpo/formulario/botao/botao.js
+++ exemplo/js/componentes/corpo/formulario/botao/botao.js
@@ -1,6 +1,14 @@
 import { carregar } from "../../../carregar.js";
 import { enviarDados, imprimeObjetoJson } from "../../../../api/criar.js";
 
+function lerDados() {
+  const corpoPainelDeSaida = document.getElementById("corpoPainelDeSaida");
+  const nome = document.getElementById("nome").value;
+  const nascimento = document.getElementById("nascimento").value;
+  const email = document.getElementById("email").value;
+  return { nome, nascimento, email };
+}
+
 export async function componenteBotao() {
   const formulario = document.querySelector('#formulario');
   await carregar('./js/componentes/corpo/formulario/botao/botao.html', formulario);
@@ -8,9 +16,8 @@ export async function componenteBotao() {
   const botao = container.querySelector('button');
   botao.addEventListener('click', async (event) => {
     event.preventDefault();
-    const corpoPainelDeSaida = document.getElementById("corpoPainelDeSaida");
-    var json = await enviarDados();
+    var dados = lerDados();
+    var json = await enviarDados(dados);
     corpoPainelDeSaida.innerHTML = imprimeObjetoJson(json);
   });
 }
-
