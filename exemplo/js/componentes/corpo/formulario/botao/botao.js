3b2922a105ed2c47f5a351e6cacb624ce6e1c413
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-05-28 04:33:50 +0000

    Exemplo

--- exemplo/js/componentes/corpo/formulario/botao/botao.js
+++ exemplo/js/componentes/corpo/formulario/botao/botao.js
@@ -1,12 +1,18 @@
 import { carregar } from "../../../carregar.js";
 import { enviarDados, imprimeObjetoJson } from "../../../../api/criar.js";
 
-function lerDados() {
-  const corpoPainelDeSaida = document.getElementById("corpoPainelDeSaida");
+function lerDadosFormulario() {
   const nome = document.getElementById("nome").value;
   const nascimento = document.getElementById("nascimento").value;
   const email = document.getElementById("email").value;
+
   return { nome, nascimento, email };
+}
+
+function limparDadosFormulario() {
+  document.getElementById("nome").value = "";
+  document.getElementById("nascimento").value = "";
+  document.getElementById("email").value = "";
 }
 
 export async function componenteBotao() {
@@ -16,8 +22,13 @@ export async function componenteBotao() {
   const botao = container.querySelector('button');
   botao.addEventListener('click', async (event) => {
     event.preventDefault();
-    var dados = lerDados();
+
+    var dados = lerDadosFormulario();
     var json = await enviarDados(dados);
+
+    const corpoPainelDeSaida = document.getElementById("corpoPainelDeSaida");
     corpoPainelDeSaida.innerHTML = imprimeObjetoJson(json);
+    
+    limparDadosFormulario();
   });
 }
